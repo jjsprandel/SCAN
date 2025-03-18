6a777cc6ae1cb8f06a14597326fc55e9f5ca351a
Jonah Sprandel <jjsprandel@gmail.com>
2025-03-18 07:31:17 +0000

    still not working

--- src/app/i2c/include/i2c_config.h
+++ src/app/i2c/include/i2c_config.h
@@ -1,7 +1,8 @@
+#ifndef I2C_CONFIG_H
+#define I2C_CONFIG_H
+
 #include <stdio.h>
 #include <stdint.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "esp_log.h"
 #include "driver/i2c_master.h"
 
@@ -20,3 +21,4 @@ extern i2c_master_bus_handle_t master_handle;
 void i2c_master_init(i2c_master_bus_handle_t *bus_handle);
 void i2c_master_add_device(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *dev_handle, const i2c_device_config_t *dev_config);
 
+#endif
